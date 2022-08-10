f1c5b0b77cf71783d3fe6115a5de804a8197e559
Li <xiaomeng.li@edu.sait.ca>
2022-08-10 09:22:41 -0600

    Add mobible navigation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
 // Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
